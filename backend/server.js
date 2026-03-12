ff131944f7ab743cde211c937a4302227cc19e87
nicoledd <nicole.dong@gmail.com>
2026-03-12 19:36:54 -0400

    add /

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Respond to preflight requests for all routes
-app.options("*", cors(corsOptions));
+app.options("/*", cors(corsOptions));
 
 app.post('/signup', async(req, res) => {
   const { username, password } = req.body;
